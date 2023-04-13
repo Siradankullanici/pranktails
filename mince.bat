0b2760aed7e55c1bf261384f8e33a8b79f5f4cf6
Demir <130374789+Siradankullanici@users.noreply.github.com>
2023-04-13 10:24:37 +0300

    Add files via upload

--- mince.bat
+++ mince.bat
@@ -1,4 +1,4 @@
 @echo off
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
-REG ADD HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Stroage Device Polices
+REG ADD HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Stroage Device Polices /f
 pause
