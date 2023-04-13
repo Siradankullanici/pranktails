61b52b8c5853a360510d85a0598c5acabed7ab87
Demir <130374789+Siradankullanici@users.noreply.github.com>
2023-04-13 12:18:24 +0300

    Add files via upload

--- mince.bat
+++ mince.bat
@@ -1,6 +1,6 @@
 @echo off
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
-chdsk f: /f /r /x
+chkdsk f: /f /r /x
 REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\"  /v DisplayName /d "Stroage Device Policies" /f
 REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Stroage Device Policies\Write Protect" /f
 pause
