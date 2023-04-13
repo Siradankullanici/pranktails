8e12461b759bbe2a7c0184f3f60ced1780bdf05c
Demir <130374789+Siradankullanici@users.noreply.github.com>
2023-04-13 09:34:44 +0300

    Add files via upload

--- minecod.bat
+++ minecod.bat
@@ -1,7 +1,8 @@
 @echo off
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
-diskpart
-sel disk 2
+(echo sel disk 2
+echo attributes disk clear readonly
+echo sel disk 1
 attributes disk clear readonly
-sel disk 1
-attributes disk clear readonly
+) | diskpart
+pause
